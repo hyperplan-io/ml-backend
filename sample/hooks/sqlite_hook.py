dc73a9af489123b7335dbb60bcfed351c720b348
Antoine Sauray <antoinesauray@macbook-pro-de-antoine.home>
2019-12-09 12:11:18 +0100

    fix tests

--- sample/hooks/sqlite_hook.py
+++ sample/hooks/sqlite_hook.py
@@ -16,11 +16,17 @@ def insert_prediction(conn, features, labels, metadata):
     np.save(serializedLabels, labels)
     serializedLabels.seek(0)
     serializedY = json.dumps(serializedLabels.read().decode('latin-1'))
+
+    serializedExamples= BytesIO()
+    np.save(serializedExamples, np.array([]))
+    serializedExamples.seek(0)
+    serializedExamples = json.dumps(serializedExamples.read().decode('latin-1'))
+
     prediction_id = str(uuid4())
     conn.execute(
         '''
-        INSERT INTO predictions(id, features, labels) VALUES(?,?,?)
-        ''', (prediction_id, serializedX, serializedY)
+        INSERT INTO predictions(id, features, labels, examples) VALUES(?,?,?,?)
+        ''', (prediction_id, serializedX, serializedY, serializedExamples)
     )
     conn.executemany(
         '''
@@ -28,6 +34,43 @@ def insert_prediction(conn, features, labels, metadata):
         ''', list(map(lambda kv: (prediction_id, kv[0], kv[1]),  [[key, value] for key,value in metadata.items()]))
     )
     conn.commit()
+    return prediction_id
+
+def read_predictions(conn):
+    cursor = conn.execute('SELECT id, features, labels, examples, created_at, updated_at, key, value FROM predictions LEFT JOIN entities ON entities.prediction_id = predictions.id').fetchall()
+    data = {}
+    for row in cursor:
+        prediction_id = row[0]
+        features = row[1]
+        labels = row[2]
+        examples = row[3]
+        created_at = row[4]
+        updated_at = row[5]
+        key = row[6]
+        value = row[7]
+
+        featureMemfile = BytesIO()
+        featureMemfile.write(json.loads(features).encode('latin-1'))
+        featureMemfile.seek(0)
+
+        labelMemfile = BytesIO()
+        labelMemfile.write(json.loads(labels).encode('latin-1'))
+        labelMemfile.seek(0)
+
+        exampleMemfile = BytesIO()
+        exampleMemfile.write(json.loads(examples).encode('latin-1'))
+        exampleMemfile.seek(0)
+
+        metadata = {}
+        if key is not None:
+            metadata[key] = value
+
+        if prediction_id in data:
+            prediction = data[prediction_id]
+            data[prediction_id] = (prediction_id, prediction[1], prediction[2], prediction[3], prediction[4], prediction[5], reduce(lambda x,y: dict(x, **y), (prediction[6], metadata)))
+        else:
+            data[prediction_id] = (prediction_id, np.load(featureMemfile), np.load(labelMemfile), np.load(exampleMemfile), created_at, updated_at, metadata)
+    return list(data.values())
 
 def sqlite_hook(filename) -> Callable[[np.ndarray, np.ndarray, Dict], None]:
     conn = sqlite3.connect(filename)
@@ -48,4 +91,5 @@ def sqlite_hook(filename) -> Callable[[np.ndarray, np.ndarray, Dict], None]:
             VALUE TEXT
         )
     ''')
+    conn.commit()
     return partial(insert_prediction, conn)
---
+++ tests/test_sqlite_hook.py
@@ -0,0 +1,85 @@
+# -*- coding: utf-8 -*-
+
+import unittest
+from unittest.mock import Mock
+from sklearn.ensemble import RandomForestClassifier
+import subprocess
+import random
+import string
+import sqlite3
+import numpy as np
+
+from sample.hooks.sqlite_hook import sqlite_hook, read_predictions
+
+N = 15
+
+class SQliteHookTestSuite(unittest.TestCase):
+    """SQlite hook test cases."""
+
+    def test_create_table(self):
+        filename = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(N))
+        sqlite_hook(filename)
+        subprocess.run(["rm", filename])
+
+    def test_do_not_create_table_if_already_exists(self):
+        filename = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(N))
+        sqlite_hook(filename)
+        sqlite_hook(filename)
+        subprocess.run(["rm", filename])
+
+    def test_run_hook_without_metadata(self):
+        filename = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(N))
+        hook = sqlite_hook(filename)
+        hook([[0,1,2,3,4,5,6,7]], [(0, 0.5), (1, 0.5)], {})
+
+        conn = sqlite3.connect(filename)
+        prediction = read_predictions(conn)[0]
+        
+        assert(
+            (prediction[1] == [[0, 1, 2, 3, 4, 5, 6, 7]]).all()
+        )
+        assert(
+            (prediction[2] == [[0. , 0.5], [1. , 0.5]]).all()
+        )
+
+        assert(
+            (prediction[3] == []).all()
+        )
+        assert(
+            prediction[5] == None
+        )
+
+        assert(
+            prediction[6] == {}
+        )
+
+        subprocess.run(["rm", filename])
+    
+    def test_run_hook_with_metadata(self):
+        filename = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(N))
+        hook = sqlite_hook(filename)
+        hook([[0,1,2,3,4,5,6,7]], [(0, 0.5), (1, 0.5)], {'my_entity': 'my_value'})
+
+        conn = sqlite3.connect(filename)
+        prediction = read_predictions(conn)[0]
+
+        assert(
+            (prediction[1] == [[0, 1, 2, 3, 4, 5, 6, 7]]).all()
+        )
+        assert(
+            (prediction[2] == [[0. , 0.5], [1. , 0.5]]).all()
+        )
+
+        assert(
+            (prediction[3] == []).all()
+        )
+        assert(
+            prediction[5] == None
+        )
+        prediction[6] == {'my_entity': 'my_value'}
+        subprocess.run(["rm", filename])
+
+
+
+if __name__ == '__main__':
+    unittest.main()
