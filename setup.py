3a1705baf0c13094650b3a28a49c377d9fec25d8
Antoine Sauray <antoinesauray@macbook-pro-de-antoine.home>
2019-12-10 11:23:20 +0100

    fix for release

--- setup.py
+++ setup.py
@@ -2,23 +2,28 @@
 
 # Learn more: https://github.com/kennethreitz/setup.py
 
-from setuptools import setup, find_packages
+import setuptools
 from version import get_version
 
 
 with open('README.rst') as f:
     readme = f.read()
 
-setup(
+setuptools.setup(
     name='mlbackend',
     version=get_version(),
     description='machine learning backend framework',
     long_description=readme,
     long_description_content_type='text/x-rst',
     author='Antoine Sauray',
-    author_email='sauray.antoine@outlook.com',
+    author_email='antoine@hyperplan.io',
     url='https://github.com/hyperplan-io/ml-backend',
     license='MIT',
-    packages=find_packages(exclude=('tests', 'docs'))
+    packages=[
+        'mlbackend',
+        'mlbackend.hooks',
+    ],
+    package_dir={'mlbackend': 'mlbackend/', 'hooks': 'mlbackend/hooks/'},
+    include_package_data=True
 )
 
